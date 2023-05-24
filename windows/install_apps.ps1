03911131f6a8432dc8c11d1164cf27e79ec9ee73
ranokay <yuuta.dev@gmail.com>
2023-05-24 13:35:50 +0300

    added install apps script

--- windows/install_apps.ps1
+++ windows/install_apps.ps1
@@ -1,31 +1,31 @@
-# # Sourcing the config file to get the functions
-# . .\config_shell.ps1
+# Sourcing the config file to get the functions
+. .\config_shell.ps1
 
-# RunAsAdmin
+RunAsAdmin
 
-# if (UserConfirm "Do you want to install Visual Studio Code?") {
-#     InstallWithWinget "Visual Studio Code" "Microsoft.VisualStudioCode"
-# }
+if (UserConfirm "Do you want to install Visual Studio Code?") {
+    InstallWithWinget "Visual Studio Code" "Microsoft.VisualStudioCode"
+}
 
-# if (UserConfirm "Do you want to install Brave Browser?") {
-#     InstallWithWinget "Brave Browser" "BraveSoftware.BraveBrowser"
-# }
+if (UserConfirm "Do you want to install Brave Browser?") {
+    InstallWithWinget "Brave Browser" "BraveSoftware.BraveBrowser"
+}
 
-# # Install Chocolatey if it's not already installed
-# function InstallChoco ($command) {
-# 	if (-not (Get-Command $command -ErrorAction SilentlyContinue)) {
-# 		Write-Host "Installing $command" -ForegroundColor Green
-# 		[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-# 		try {
-# 			iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-# 		} catch {
-# 			Write-Host "Failed to install $command. Error: $_" -ForegroundColor Red
-# 		}
-# 	}
-# }
+# Install Chocolatey if it's not already installed
+function InstallChoco ($command) {
+	if (-not (Get-Command $command -ErrorAction SilentlyContinue)) {
+		Write-Host "Installing $command" -ForegroundColor Green
+		[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+		try {
+			iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+		} catch {
+			Write-Host "Failed to install $command. Error: $_" -ForegroundColor Red
+		}
+	}
+}
 
-# # Software to be installed with Choco
-# $chocoSoftwareList = "choco"
-# if (UserConfirm "Do you want to install $chocoSoftwareList?") {
-# 	InstallWithChoco $chocoSoftwareList
-# }
+# Software to be installed with Choco
+$chocoSoftwareList = "choco"
+if (UserConfirm "Do you want to install $chocoSoftwareList?") {
+	InstallWithChoco $chocoSoftwareList
+}
