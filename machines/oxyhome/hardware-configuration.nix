c0bbee17ed3e80986c8b2cf42aa54722fc0b0b05
ranokay <github@ranokay.com>
2024-09-08 21:08:07 +0300

    Update hardware-configuration.nix for x86_64-linux host platform

--- machines/oxyhome/hardware-configuration.nix
+++ machines/oxyhome/hardware-configuration.nix
@@ -48,7 +48,6 @@
   };
 
   networking.useDHCP = lib.mkDefault true;
-  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
