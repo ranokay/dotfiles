7ed329108dcc49d7a050fa575fbef102ee42e804
ranokay <github@ranokay.com>
2024-09-09 09:54:28 +0300

    Refactor Caddy configuration to remove unused code and secrets

--- services/caddy.nix
+++ services/caddy.nix
@@ -2,70 +2,72 @@
   config,
   pkgs,
   ...
-}: let
-  caddyWithPlugins = pkgs.caddy.override {
-    buildGoModule = args:
-      pkgs.buildGoModule (args
-        // {
-          vendorSha256 = null;
-          overrideModAttrs = _: {
-            postBuild = ''
-              go install github.com/caddy-dns/cloudflare@latest
-            '';
-          };
-        });
-  };
-in {
+}:
+# let
+#   caddyWithPlugins = pkgs.caddy.override {
+#     buildGoModule = args:
+#       pkgs.buildGoModule (args
+#         // {
+#           vendorSha256 = null;
+#           overrideModAttrs = _: {
+#             postBuild = ''
+#               go install github.com/caddy-dns/cloudflare@latest
+#             '';
+#           };
+#         });
+#   };
+# in
+{
   services.caddy = {
     enable = true;
-    package = caddyWithPlugins;
-    configFile = pkgs.writeText "Caddyfile" ''
-      (cloudflare) {
-        tls {
-          dns cloudflare {
-            zone_token {$env.cloudflare_zone_key}
-            api_token {$env.cloudflare_api_key}
-          }
-        }
-      }
+    # package = caddyWithPlugins;
+    # configFile = pkgs.writeText "Caddyfile" ''
+    #   (cloudflare) {
+    #     tls {
+    #       dns cloudflare {
+    #         zone_token {$env.cloudflare_zone_key}
+    #         api_token {$env.cloudflare_api_key}
+    #       }
+    #     }
+    #   }
 
-      # navidrome
-      # navidrome.proxmox.oxystack.com {
-      #   reverse_proxy http://192.168.0.102:4533
-      #   import cloudflare
-      # }
+    #   # navidrome
+    #   # navidrome.proxmox.oxystack.com {
+    #   #   reverse_proxy http://192.168.0.102:4533
+    #   #   import cloudflare
+    #   # }
 
-      # nextcloud
-      nextcloud.home.oxystack.com {
-        reverse_proxy https://192.168.0.100:8080 {
-          transport http {
-            tls_insecure_skip_verify
-          }
-        }
-        import cloudflare
-      }
+    #   # nextcloud
+    #   nextcloud.home.oxystack.com {
+    #     reverse_proxy https://192.168.0.100:8080 {
+    #       transport http {
+    #         tls_insecure_skip_verify
+    #       }
+    #     }
+    #     import cloudflare
+    #   }
 
-      # huawei
-      huawei.home.oxystack.com {
-        reverse_proxy http://192.168.0.1
-        import cloudflare
-      }
-    '';
+    #   # huawei
+    #   huawei.home.oxystack.com {
+    #     reverse_proxy http://192.168.0.1
+    #     import cloudflare
+    #   }
+    # '';
   };
 
-  environment.systemPackages = [caddyWithPlugins];
+  # environment.systemPackages = [caddyWithPlugins];
 
-  sops.secrets = {
-    cloudflare_zone_key = {};
-    cloudflare_api_key = {};
-  };
+  # sops.secrets = {
+  #   cloudflare_zone_key = {};
+  #   cloudflare_api_key = {};
+  # };
 
-  systemd.services.caddy.serviceConfig = {
-    EnvironmentFile = [
-      config.sops.secrets.cloudflare_zone_key.path
-      config.sops.secrets.cloudflare_api_key.path
-    ];
-  };
+  # systemd.services.caddy.serviceConfig = {
+  #   EnvironmentFile = [
+  #     config.sops.secrets.cloudflare_zone_key.path
+  #     config.sops.secrets.cloudflare_api_key.path
+  #   ];
+  # };
 
   # environment.persistence."/nix/persist" = {
   #   directories = [
