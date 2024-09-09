040e0b1a2e9efb9b68ee47c76b6bde02bc781567
ranokay <github@ranokay.com>
2024-09-09 09:55:21 +0300

    Refactor auto-cpufreq module and configuration

--- modules/nixos/auto-cpufreq.nix
+++ modules/nixos/auto-cpufreq.nix
@@ -1,19 +1,15 @@
-# configuration.nix
-
 {inputs, pkgs, ...}: {
-    # ---Snip---
-    programs.auto-cpufreq.enable = true;
-    # optionally, you can configure your auto-cpufreq settings, if you have any
-    programs.auto-cpufreq.settings = {
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-
-    battery = {
-      governor = "powersave";
-      turbo = "auto";
+  programs.auto-cpufreq = {
+    enable = true;
+    settings = {
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+      battery = {
+        governor = "powersave";
+        turbo = "auto";
+      };
     };
   };
-    # ---Snip---
 }
